d633a04bd5e2c95f8a066c417fc4f3448ada3ea5
Aditya Bhosale <147322292+AdityaBhosale22@users.noreply.github.com>
2026-01-29 16:24:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+const output = document.getElementById("output");
+
+function magic(){
+	const data = [1,2,3,4];
+	new Promise((resolve)=>{
+		setTimeout(()=>{
+			const filtered = data.filter(num => num % 2 === 0);
+			output.innerText = filtered.join(",");
+			resolve(filtered);
+		},1000)
+	})
+	.then((filteredArray)=>{
+		return new Promise((resolve)=>{
+			setTimeout(() => {
+                const multiplied = filteredData.map(num => num * 2);
+                output.innerText = multiplied.join(","); 
+                resolve(multiplied);
+            }, 2000);
+		})
+	})
+}
+manipulateArray();
